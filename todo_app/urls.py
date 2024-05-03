3c12932032396a7d6b13b215aa9d1ef2cb5914e8
Illya Maznitskiy <comercaleuros@gmail.com>
2024-05-03 16:50:50 +0300

    add Task and Tag urls

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,51 @@
-from django.urls import path, include
+from django.urls import path
 
-from todo_app import views
+from todo_app.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView, toggle_do_undo,
+)
+
+
+app_name = "todo_app"
 
 urlpatterns = [
-    path('', include("todo_app.urls"), namespace="todo_app"),
+    path("", TaskListView.as_view(), name="task-list"),
+    path(
+        "create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        'tasks/<int:pk>/update/',
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        'tasks/<int:pk>/delete/',
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path("<int:pk>/toggle/", toggle_do_undo, name="toggle-task"),
+    path("tags", TagListView.as_view(), name="tag-list"),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create"
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete"
+    ),
 ]
