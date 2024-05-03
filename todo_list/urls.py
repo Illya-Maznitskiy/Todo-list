d6d65ded3e20735c0b7148f56af519ecf2700506
Illya Maznitskiy <comercaleuros@gmail.com>
2024-05-03 15:07:35 +0300

    fix home page in urls.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from todo_app import views
 
+app_name = "todo_app"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="home"),
+    path("", include("todo_app.urls", namespace="todo_app"))
 ]
-
-app_name = "todo_list"
