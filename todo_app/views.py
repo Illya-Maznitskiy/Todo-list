60df4d338072a1a14a18625ddc0d6720ef569335
Illya Maznitskiy <comercaleuros@gmail.com>
2024-05-03 16:55:58 +0300

    add Task, Tag views and toggle_do_undo func in views.py

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,10 +1,60 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import generic
+
+from todo_app.forms import TaskForm
+from todo_app.models import Task, Tag
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "todo_app/index.html")
+class TaskListView(generic.ListView):
+    model = Task
 
 
-def home(request):
-    return None
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_app:task_list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    template_name = "todo_app/task_form.html"
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_app:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo_app/task_confirm_delete.html"
+    success_url = reverse_lazy("todo_app:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+def toggle_do_undo(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.is_completed:
+        task.is_completed = False
+    else:
+        task.is_completed = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("todo_app:task-list"))
