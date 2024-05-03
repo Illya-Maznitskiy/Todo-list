786f35dc04333fe2fcc315c7b2f9cd604e2f3007
Illya Maznitskiy <comercaleuros@gmail.com>
2024-05-03 15:05:37 +0300

    update models.py with meta classes and __str__ methods

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Task(models.Model):
     content = models.TextField()
@@ -11,3 +17,9 @@ class Task(models.Model):
     deadline = models.DateTimeField(null=True, blank=True)
     done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ["done", "-datetime", ]
+
+    def __str__(self):
+        return self.content
