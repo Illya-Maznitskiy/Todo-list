705a3f07efa285ebb40e79669e5a088d34eaf10d
Illya Maznitskiy <comercaleuros@gmail.com>
2024-05-04 14:15:37 +0300

    update deadline and content forms

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -5,8 +5,7 @@ from todo_app.models import Tag, Task
 
 class TaskForm(forms.ModelForm):
     deadline = forms.DateTimeField(
-        widget=forms.DateTimeInput(),
-        help_text="Example: 2024-01-01 23:59",
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
         required=False
     )
     tags = forms.ModelMultipleChoiceField(
@@ -16,4 +15,11 @@ class TaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
-        fields = "__all__"
+        exclude = ["done"]
+        widgets = {
+            "content": forms.TextInput(attrs={"size": "30"}),
+        }
+
+        help_texts = {
+            "content": "Write your task",
+        }
